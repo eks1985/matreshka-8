3eed6381605714ccd88ea78ecad6b9487728f2eb
Efimov Kirill <ya.kefimov@yandex.ru>
2017-02-27 07:31:04 +0000

    bg fixed size

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ class App extends Component {
     return (
       <div className="App" style={{ position: 'relative' }}>
         <div style={{ position: 'relative', display: 'flex', width: '100%', justifyContent: 'center' }}>
-          <div style={{ position: 'absolute', bottom: '465px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>О нас</div>
-          <div style={{ position: 'absolute', bottom: '390px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Контакты</div>
-          <div style={{ position: 'absolute', bottom: '300px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Коллектив</div>
-          <div style={{ position: 'absolute', bottom: '215px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Родителям</div>
-          <div style={{ position: 'absolute', bottom: '126px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Документы</div>
-          <div style={{ position: 'absolute', bottom: '40px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Новости</div>
+          <div style={{ position: 'absolute', bottom: '485px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>О нас</div>
+          <div style={{ position: 'absolute', bottom: '405px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Контакты</div>
+          <div style={{ position: 'absolute', bottom: '315px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Коллектив</div>
+          <div style={{ position: 'absolute', bottom: '222px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Родителям</div>
+          <div style={{ position: 'absolute', bottom: '130px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Документы</div>
+          <div style={{ position: 'absolute', bottom: '42px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Новости</div>
         </div>
       </div>
     );
