684bdd94869de82387bdcddad229b61aa68a67e5
Efimov Kirill <ya.kefimov@yandex.ru>
2017-02-27 07:10:26 +0000

    added menu prototype

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="App" style={{ position: 'relative' }}>
+        <div style={{ position: 'relative', display: 'flex', width: '100%', justifyContent: 'center' }}>
+          <div style={{ position: 'absolute', bottom: '465px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>О нас</div>
+          <div style={{ position: 'absolute', bottom: '390px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Контакты</div>
+          <div style={{ position: 'absolute', bottom: '300px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Коллектив</div>
+          <div style={{ position: 'absolute', bottom: '215px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Родителям</div>
+          <div style={{ position: 'absolute', bottom: '126px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Документы</div>
+          <div style={{ position: 'absolute', bottom: '40px', color: '#eee', fontSize: '24px', cursor: 'pointer' }}>Новости</div>
+        </div>
       </div>
     );
   }
